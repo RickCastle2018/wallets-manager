b2f7a22cc8d51896f149ed60d1a72fbb73c6d267
Nikita Nikonov <nnikonov2006@gmail.com>
2021-08-19 00:52:17 +0300

    fixed user-wallet api spec

--- service/server.js
+++ service/server.js
@@ -1,13 +1,15 @@
+// TODO: .gitignore ignore db
+
 'use strict';
 
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 2311;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`wallets-manager running at http://localhost:${port}`)
 })
