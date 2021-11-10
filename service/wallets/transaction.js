ae786bb80c9e353e68f747ee22b5b8fed477aa04
Nikita Nikonov <nnikonov2006@gmail.com>
2021-11-10 23:28:42 +0300

    nonce management ready

--- service/wallets/transaction.js
+++ service/wallets/transaction.js
@@ -10,9 +10,24 @@ export const txStorage = new NodeCache({
   checkperiod: 150
 })
 
-// export const nonceCache = new NodeCache({
-//
-// })
+export const nonceCache = new NodeCache({
+  checkperiod: 0,
+  deleteOnExpire: false
+})
+
+function getAndIncrementNonce (address, callback) {
+  const nonce = nonceCache.take(address)
+  if (nonce !== undefined) {
+    callback(parseInt(nonce))
+    nonceCache.set(address, (parseInt(nonce) + 1).toString())
+    return
+  }
+  web3.eth.getTransactionCount(address, 'pending')
+    .then((txCount) => {
+      callback(parseInt(txCount))
+      nonceCache.set(address, (parseInt(txCount) + 1).toString())
+    })
+}
 
 const common = Common.default.forCustomChain('mainnet', {
   name: 'bnb',
@@ -39,7 +54,7 @@ export default class Tx {
   }
 
   execute () {
-    web3.eth.getTransactionCount(this.txObject.from, (e, txCount) => {
+    getAndIncrementNonce(this.txObject.from, (txCount) => {
       this.txObject.nonce = txCount
 
       const tx = ethertx.Transaction.fromTxData(this.txObject, {
@@ -70,6 +85,14 @@ export default class Tx {
             data: webhook
           })
         }).on('error', (error) => {
+          web3.eth.getTransactionCount(this.txObject.from, 'pending')
+            .then((txCount) => {
+              const nonce = nonceCache.take(this.txObject.from)
+              if (nonce !== txCount) {
+                nonceCache.set(this.txObject.from, txCount.toString())
+              }
+            })
+
           const webhook = {
             transaction_id: this.id,
             type: 'internal',
