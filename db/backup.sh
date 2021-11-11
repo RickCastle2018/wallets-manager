df61f53642fe31c539ef9c135d16f53417b92fb2
Nikita Nikonov <nnikonov2006@gmail.com>
2021-11-11 18:06:00 +0300

    backups fix

--- db/backup.sh
+++ db/backup.sh
@@ -1,8 +1,7 @@
-# based on https://dev.to/mkubdev/mongodump-and-mongorestore-with-docker-39m7
-# usage: sudo sh backup.sh
+# usage: sudo sh backup.sh {db-name}
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 BACKUP_DIR="$SCRIPT_DIR/backups"
 [[ ! -d "$BACKUP_DIR" ]] && mkdir -p "$BACKUP_DIR"
-FILENAME="$(date '+%Y-%m-%d-%H:%M:%S').dump"
-docker-compose exec -T db sh -c 'mongodump --archive' > "${BACKUP_DIR}"/"${FILENAME}"
+FILENAME="$(date '+%Y-%m-%d-%H:%M:%S')-$1.dump"
+docker-compose exec -T db sh -c "mongodump --username ogle --password nikita --authenticationDatabase $1 --archive" > "${BACKUP_DIR}"/"${FILENAME}"
