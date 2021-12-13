8d177a382dd682c982e72eabb5d5574a42125aaf
Nikita Nikonov <nnikonov2006@gmail.com>
2021-12-13 17:47:05 +0300

    bug found

--- service/api/transactions.js
+++ service/api/transactions.js
@@ -1,5 +1,4 @@
 import { txStorage } from '../wallets/transaction.js'
-import logger from '../utils/logger.js'
 
 export function middleware (req, res, next) {
   const tx = txStorage.get(req.params.txId)
@@ -21,14 +20,5 @@ export function cancel (req, res) {
 
 export function proceed (req, res) {
   req.tx.execute()
-
-  logger.info(req.tx.id)
-
-  // let oldData = req.tx.data
-  // req.tx.enqueue({
-  //   oldData
-  //
-  // })
-
-  res.status(200).send('OK')
+  res.status(200).send()
 }
--- service/wallets/commissionexchange.js
+++ service/wallets/commissionexchange.js
@@ -5,6 +5,7 @@ export default function commissionExchange (tx, uW, cb) {
   const bnbFee = new BigNumber(tx.data.fee.bnb)
   const coinAmount = bnbFee.multipliedBy(2).multipliedBy(parseInt(process.env.BNB_PRICE))
 
+  // TODO: FIX THIS SHIT
   const idArr = [tx.id + 'c', tx.id + 'b']
   exchange(idArr, uW, coinAmount.toString(), 'oglc',
     (err) => {
